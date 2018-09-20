dce0b4f002fe586a863fea0118dd88ba0d975956
cchadwick <cchadwick@mastercontrol.com>
2018-09-20 08:54:57 -0600

    Stage 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import {Injectable, NgModule} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import {Observable} from '../../node_modules/rxjs';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
